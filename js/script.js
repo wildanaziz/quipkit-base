f45d3246faf1dfbf2ad8eca9fcd1375132c861de
Wildan Aziz <danitatadanwildan@gmail.com>
2024-07-11 14:12:09 +0700

    update responsive 1.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuBtnIcon = document.querySelector('.menu-btn i');
+const linkMenuBtn = document.querySelector('.link-hero-menu');
+
+menuBtn.onclick = function() {
+    linkMenuBtn.classList.toggle('open');
+
+    const isOpen = linkMenuBtn.classList.contains('open');
+    
+    menuBtnIcon.textContent = isOpen
+        ? 'close'
+        : 'menu'
+}
+
 const btnScrollToTop = document.querySelector("#btnScrollToTop");
 
 btnScrollToTop.addEventListener("click", function () {
@@ -6,4 +20,5 @@ btnScrollToTop.addEventListener("click", function () {
         left: 0,
         behavior: "smooth"
     });
-})
+});
+
